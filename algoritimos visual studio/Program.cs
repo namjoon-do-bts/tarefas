e211a49ce01343451d09236fb6da27e38c36e64a
namjoon-do-bts <flor.l@escolar.ifrn.edu.br>
2024-08-12 16:09:32 -0300

    arquivos de visual studio

---
+++ algoritimos visual studio/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+string colega1 = "fiona";
+string colega2 = "gabirel";
+
+int nota1 = 10;
+int nota2 = 50;
+
+Console.WriteLine($"{colega1} tirou {nota1} na prova e {colega2} tirou {nota2}");
+*/
+/*
+string colega1 = "Namjoon";
+string colega2 = "Jin";
+
+Console.WriteLine($"{colega1} e {colega2} são casados ");
+*/
+/*
+int a = 18;
+int b = 6;
+int c;
+
+c = a + b;
+Console.WriteLine("soma: " + c);
+c = a - b;
+Console.WriteLine("subtração: " + c);
+c = a * b;
+Console.WriteLine("multiplicação: "+ c);
+c = a / b;
+Console.WriteLine("divisão: " + c);
+*/
+
+
+/*
+int a = 20; 
+int b = 7;
+int quociente = a / b;
+int resto = a % b;
+
+Console.WriteLine($"{a} dividido por {b}");
+
+Console.WriteLine("Quociente: " + quociente);
+
+Console.WriteLine("Resto: " + resto);
+*/
+
+/*
+var a  = 5;
+var b = 4.8769f;
+var c = 9.000000000000000;
+
+var d = a + b + c;
+
+var mensagem1 = "Resultado da soma ";
+var caractere = 'd';
+
+var mensagem2 = mensagem1 + caractere + " é igual a: ";
+
+Console.WriteLine(mensagem2 + d);
+*/
+
+/*
+var x = 5;
+var y = 2;
+
+var z = x / y;
+
+Console.WriteLine(z);
+*/
+/*
+bool p = (7 <= 3) && (1+1 == 2);
+bool q = 10/2 !== 5;
+
+Console.WriteLine(!p);
+Console.WriteLine(!q);
+Console.WriteLine(p && q);
+Console.WriteLine(p || q);
+*/
+
+
+
+
+
+
